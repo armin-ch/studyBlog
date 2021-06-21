f48e96cb0ca9969de0af0513400482147debd5d2
diaseu <andtheregoesmeow@gmail.com>
2021-06-20 20:53:24 -0700

    updated postRoutes.js to env

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 const { Post } = require('../models')
 const passport = require('passport')
 const mysql = require('mysql2')
-const db = mysql.createConnection('mysql://root:rootroot@localhost:8809/studyblog_db')
+const db = mysql.createConnection(process.env.LOCALDB_URL)
 
 router.get('/posts', (req, res) => {
   db.query('SELECT * FROM posts', (err, posts) => {
